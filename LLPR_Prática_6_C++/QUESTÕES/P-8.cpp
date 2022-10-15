d452fe563522969e477f0a7812f389f100b7b22b
Guilherme Bandeira <107276182+Asphyan@users.noreply.github.com>
2022-10-15 00:11:55 -0300

    Add files via upload

--- LLPR_Prática_6_C++/QUESTÕES/P-8.cpp
+++ LLPR_Prática_6_C++/QUESTÕES/P-8.cpp
@@ -9,7 +9,7 @@ int main() {
     printf("Digite que horas são, no formato 24 horas (números entre 00 e 24): ");
     scanf("%d", &hora);
 
-    if((hora >= 5)){
+    if((hora <= 18)&&(hora >=5)){
         printf("ESCURO!");
     }
     return 0;
