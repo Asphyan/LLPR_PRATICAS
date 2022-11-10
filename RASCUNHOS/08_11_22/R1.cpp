fccc4b96cb8f733b6a368ea164387106f952dffa
Guilherme Bandeira <107276182+Asphyan@users.noreply.github.com>
2022-11-10 14:59:34 -0300

    Add files via upload

--- RASCUNHOS/08_11_22/R1.cpp
+++ RASCUNHOS/08_11_22/R1.cpp
@@ -3,9 +3,13 @@
 int main () {
 	int x;
 	
-	for (int x=1; x <= 1000; x+=2 ) {
-		printf("%d\n\n", x);
-		
+	for (int x=1; x <= 59; x=x+1 ) {
+		if (x%2 == 0){
+			printf("PPP%d: ", x);
+		}
+		if (x%2 != 0){
+			printf("III%d: ", x);
+		}
 	}
 	return 0;
 }
